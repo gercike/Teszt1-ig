ef92622585889adf37d4a948a98982beb2373974
gercike <gercike13@gmail.com>
2020-11-21 00:07:16 +0100

    minor error delete

--- src/Elagazasok1.java
+++ src/Elagazasok1.java
@@ -1,25 +1,22 @@
 public class Elagazasok1 {
     public static void main(String[] args) {
-        int myAge=23;
-        if (myAge<0){
+        int myAge = 23;
+        if (myAge < 0) {
             System.out.println("Helló, Marty!");
         }
-                if (myAge>20){
-                    System.out.println("Jó napot kívánok!");
-                }
-                else {
-                    if (myAge<3){
-                        System.out.println("bru-bru-bru");
-                    }
-                    else {
-                        if(myAge>=3 && myAge<13) {
-                            System.out.println("Szióka!");
-                        }
-                        else {
-                            System.out.println("Cső!");
-                        }
-                    }
+        if (myAge > 20) {
+            System.out.println("Jó napot kívánok!");
+        } else {
+            if (myAge < 3) {
+                System.out.println("bru-bru-bru");
+            } else {
+                if (myAge >= 3 && myAge < 13) {
+                    System.out.println("Szióka!");
+                } else {
+                    System.out.println("Cső!");
                 }
             }
+        }
     }
 }
+
